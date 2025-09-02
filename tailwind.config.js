a3256786e75b38f81ab288f5adfcbad73acfd8d2
Mariya Efron Y <mariyaefrony@gmail.com>
2025-09-02 09:40:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // adjust to your project
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#2563eb",   // Blue
-        secondary: "#9333ea", // Purple
-        background: "#f9fafb",
-        textDark: "#111827",
-      },
-    },
-  },
-  plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 };
